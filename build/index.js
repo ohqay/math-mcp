c37183d1dd0c60bcb0e9f44777a4930ea6f7f148
Ethan Henrickosn <109119138+EthanHenrickson@users.noreply.github.com>
2025-03-01 00:22:36 -0600

    Update index.js

--- build/index.js
+++ build/index.js
@@ -17,6 +17,42 @@ server.tool("add", "Adds two numbers together", {
             }]
     };
 });
+server.tool("subtract", "Subtract numberA from numberB", {
+    numberA: z.number().describe("The number being subtracted"),
+    numberB: z.number().describe("The number subtracting")
+}, async ({ numberA, numberB }) => {
+    const total = numberA - numberB;
+    return {
+        content: [{
+                type: "text",
+                text: `${total}`
+            }]
+    };
+});
+server.tool("multiply", "Multiply numberA and numberB", {
+    numberA: z.number().describe("The first number"),
+    numberB: z.number().describe("The second number")
+}, async ({ numberA, numberB }) => {
+    const total = numberA * numberB;
+    return {
+        content: [{
+                type: "text",
+                text: `${total}`
+            }]
+    };
+});
+server.tool("division", "Divide numberB from numberA", {
+    numberA: z.number().describe("The numerator"),
+    numberB: z.number().describe("The devisor")
+}, async ({ numberA, numberB }) => {
+    const total = numberA / numberB;
+    return {
+        content: [{
+                type: "text",
+                text: `${total}`
+            }]
+    };
+});
 server.tool("sum", "Adds any number of numbers together", {
     numbers: z.array(z.number()).describe("Array of numbers to sum")
 }, async ({ numbers }) => {
@@ -25,6 +61,72 @@ server.tool("sum", "Adds any number of numbers together", {
         content: [{
                 type: "text",
                 text: `${sum}`
+            }]
+    };
+});
+server.tool("average", "Finds the average of a list of numbers", {
+    numbers: z.array(z.number()).describe("Array of numbers to find the average of")
+}, async ({ numbers }) => {
+    const sum = numbers.reduce((total, num) => total + num, 0) / numbers.length;
+    return {
+        content: [{
+                type: "text",
+                text: `${sum}`
+            }]
+    };
+});
+server.tool("min", "Finds the minimum from a list of numbers", {
+    numbers: z.array(z.number()).describe("Array of numbers to find the minimum of")
+}, async ({ numbers }) => {
+    const sum = Math.min(...numbers);
+    return {
+        content: [{
+                type: "text",
+                text: `${sum}`
+            }]
+    };
+});
+server.tool("max", "Finds the maximum from a list of numbers", {
+    numbers: z.array(z.number()).describe("Array of numbers to find the maximum of")
+}, async ({ numbers }) => {
+    const sum = Math.max(...numbers);
+    return {
+        content: [{
+                type: "text",
+                text: `${sum}`
+            }]
+    };
+});
+server.tool("floor", "Rounds a number down", {
+    numberA: z.number().describe("The number"),
+}, async ({ numberA }) => {
+    const num = Math.floor(numberA);
+    return {
+        content: [{
+                type: "text",
+                text: `${num}`
+            }]
+    };
+});
+server.tool("Ceiling", "Rounds a number up", {
+    numberA: z.number().describe("The number"),
+}, async ({ numberA }) => {
+    const num = Math.ceil(numberA);
+    return {
+        content: [{
+                type: "text",
+                text: `${num}`
+            }]
+    };
+});
+server.tool("Round", "Rounds a number", {
+    numberA: z.number().describe("The number"),
+}, async ({ numberA }) => {
+    const num = Math.round(numberA);
+    return {
+        content: [{
+                type: "text",
+                text: `${num}`
             }]
     };
 });
